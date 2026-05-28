807af626aa89cbf4e000ffc3525f76b9c8c1b76a
Marcin Krzaczyński <mkrzaczynski@gmail.com>
2026-05-28 22:35:44 +0200

    Add Netlify support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
   output: 'server',
+
   security: {
     checkOrigin: true
   },
+
   i18n: {
     locales: ['en', 'pl'],
     defaultLocale: 'en',
@@ -13,10 +16,14 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
+
   vite: {
     plugins: [tailwindcss()]
   },
+
   devToolbar: {
     enabled: false
-  }
+  },
+
+  adapter: netlify()
 });
