c557da4c293a7ae52ce5986708408cdb1351816a
Marcin Krzaczyński <mkrzaczynski@gmail.com>
2026-09-04 11:34:21 +0200

    Disable login on non production site

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
 import { defineMiddleware } from "astro:middleware";
 
 export const onRequest = defineMiddleware(async ({ cookies, url, redirect }, next) => {
+  if (!import.meta.env.PROD) { return next(); }
+
   if (url.pathname === "/login" || url.pathname === "/pl/login") {
     return next();
   }
 
   const isAuthenticated = cookies.get("site_auth")?.value === "true";
-
   if (!isAuthenticated) {
     return redirect("/login");
   }
