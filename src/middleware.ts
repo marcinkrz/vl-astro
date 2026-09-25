b40fe1cdf97a4ef55bd99d95b3b81f685425a431
Marcin Krzaczyński <mkrzaczynski@gmail.com>
2026-09-25 16:48:45 +0200

    Fix language redirect on login and cs page

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,6 +8,7 @@ export const onRequest = defineMiddleware(async ({ cookies, url, redirect }, nex
   }
 
   const isAuthenticated = cookies.get("site_auth")?.value === "true";
+
   if (!isAuthenticated) {
     return redirect("/cs");
   }
